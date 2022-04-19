452260f974fe17c95724bba3508ddc857e5d0641
Rajdoshi99 <44093439+Rajdoshi99@users.noreply.github.com>
2022-04-19 00:23:06 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,11 +24,11 @@ from aitextgen import aitextgen
 # ai = aitextgen(model_folder="model/",
 #                tokenizer_file="model/aitextgen.tokenizer.json", to_gpu=False)
 
-ai = aitextgen(model="distilgpt2", to_gpu=False)
+#ai = aitextgen(model="distilgpt2", to_gpu=False)
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
-port = 12345
+port = 1234
 base_url = get_base_url(port)
 
 
@@ -43,9 +43,95 @@ app.secret_key = os.urandom(64)
 # set up the routes and logic for the webserver
 
 
+
+
 @app.route(f'{base_url}')
 def home():
     return render_template('writer_home.html', generated=None)
+
+@app.route(f'{base_url}/results')
+def results():
+    return render_template('Write-your-story-with-AI.html')
+
+#@app.route(f'{base_url}')
+#def my_form():
+#    return render_template('index.html')
+
+@app.route(f'{base_url}/results', methods=['POST'])
+def my_form_post():
+    from flask import Flask, render_template , request 
+    from flask import jsonify
+    from transformers import GPT2LMHeadModel, GPT2Tokenizer
+    import numpy as np
+    import random
+    import torch
+    from torch.utils.data import Dataset, DataLoader
+    from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+    from tqdm import tqdm, trange
+    import torch.nn.functional as F
+    import csv
+    import os
+    from sklearn.model_selection import train_test_split
+    from aitextgen import aitextgen
+    from aitextgen.TokenDataset import TokenDataset
+    from aitextgen.utils import build_gpt2_config
+    from aitextgen.tokenizers import train_tokenizer
+    text=request.form['text']
+    prefix=text
+    '''netflix_data=pd.read_csv('netflix_titles.csv')
+    file_name = "tokenizer_input.txt"                      #Can also use df.csv() to convert it into text file.
+    with open(file_name, 'a') as f:
+        dfAsString = netflix_data['description'].to_string(header=False, index=False)
+        f.write(dfAsString)
+    training_samples = netflix_data.description.values.tolist()
+    training_file = TokenDataset(texts=training_samples, save_cache=False) #not using a custom tokenizer so redefining this variable to match that.
+    #training_file = "dataset_cache.tar.gz"
+    ai = aitextgen(tf_gpt2="124M", to_gpu=True) #Downloading the model 
+    ai.train(training_file,
+          line_by_line=False,
+          from_cache=True,
+          num_steps=3,
+          generate_every=1000,
+          save_every=1000,
+          save_gdrive=False,
+          learning_rate=1e-3,
+          fp16=False,
+          batch_size=1,
+          )'''
+
+    ai = aitextgen(model_folder="model/")
+    b=[]
+    ai.generate_to_file(n=10,
+                batch_size=1,
+                prompt=prefix,
+              temperature=0.7,
+              top_p=0.9,
+              destination_path="noob.txt",
+              prefix = '<|startoftext|>',
+              truncate='<|endoftext|>')
+
+    samples = open("noob.txt", 'r').read().split('====================')[:-1]
+    new_text = ""
+
+        # Format output
+    for i, sample in enumerate(samples):
+
+        new_text = f"{new_text } Sample {i+1} \n   {sample} \n "
+        print("-------------------------")
+
+    new_text = new_text.replace("\n", "")
+    default_prompt = prefix
+    prefix = prefix.replace("\n","</br>")
+    print(new_text)
+
+    return render_template('Write-your-story-with-AI.html', processed_text=new_text.split('Sample'))
+
+
+'''
+@app.route(f'{base_url}', methods=['POST'])
+def home_post():
+    return redirect(url_for('my_form_post'))
+
 
 
 @app.route(f'{base_url}', methods=['POST'])
@@ -81,7 +167,8 @@ def generate_text():
 
     data = {'generated_ls': generated}
     session['data'] = generated[0]
-    return redirect(url_for('results'))
+    return redirect(url_for('results'))'''
+
 
 # define additional routes here
 # for example:
@@ -92,7 +179,7 @@ def generate_text():
 
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'cocalcx.ai-camp.org'
+    website_url = 'cocalcg1.ai-camp.dev'
 
     print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
     app.run(host='0.0.0.0', port=port, debug=True)
